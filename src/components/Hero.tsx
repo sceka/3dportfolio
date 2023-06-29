cf2d642e37aa328499ef5091a0ff2325af277b0b
sceka <scekicm02@gmail.com>
2023-06-29 11:23:24 +0200

    Add Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,10 +17,28 @@ const Hero = () => {
                         Hi, I am <span className="text-[#915eff]">Marko</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I developer 3D visuals, user interfaces and web
-                        applications
+                        I developer 3D visuals, user{" "}
+                        <br className="sm:block hidden" />
+                        interfaces and web applications
                     </p>
                 </div>
+            </div>
+
+            <ComputersCanvas />
+            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
+                <a href="#about">
+                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                        <motion.dev
+                            animate={{ y: [0, 24, 0] }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className="w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+                </a>
             </div>
         </section>
     );
